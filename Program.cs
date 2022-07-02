26d1c365ec9f76c04c6e02611460e0d3891084e1
Stepan-Suprunov <ipsuprunovso@mail.ru>
2022-07-02 07:29:36 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+int [] array = new int [5];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,99);
+}
+int min = 0;
+int max = 0;
+
+void PrintArray(int [] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        Console.Write(collection[i] + " ");
+    }
+}
+
+void FindMinMax(int [] collection)
+{
+    min = collection[0];
+    for (int i = 0; i < collection.Length; i++)
+    {
+        if (collection[i] > max) max = collection[i];
+        if (collection[i] < min) min = collection[i];
+    }
+    Console.WriteLine(min + " " + max);
+}
+
+int diff = max - min;
+
+PrintArray(array);
+Console.WriteLine();
+FindMinMax(array);
+Console.WriteLine(diff);
